b358f2da78fa05662cb6be7bb4f3a98320c163cc
aprilcoffee <8765498@gmail.com>
2020-02-12 17:17:08 +0100

    update

--- scroll/sketch.js
+++ scroll/sketch.js
@@ -1,18 +1,20 @@
 var wait = [];
-var pos = 25;
-var ppos = 25; 
+//var pos = 25;
 var spread = 280;
+var pos = 0;
+var ppos = pos;
+
 function setup() {
-  createCanvas(window.innerWidth-50, window.innerHeight-50);
+  createCanvas(window.innerWidth - 50, window.innerHeight - 50);
   background(255);
-  for (var s = 0; s < 24; s++) {
-    wait.push(new Wait(width / 2,-(12*spread) + height / 2 + (s * spread), s%12));
+  for (var s = 0; s < 120; s++) {
+    wait.push(new Wait(width / 2, -(12 * spread) + height / 2 + (s * spread), s % 12, s));
   }
 }
 
 function draw() {
   background(255);
-  for (var s = 0; s <24; s++) {
+  for (var s = 0; s < 120; s++) {
     wait[s].update(pos);
     wait[s].show();
   }
@@ -23,43 +25,52 @@ function draw() {
   //line(width/2+loc,height/2-loc,width/2+loc,height/2-loc+50);
   //line(width/2-loc,height/2+loc,width/2-loc+50,height/2+loc);
   //line(width/2-loc,height/2+loc,width/2-loc,height/2+loc-50);
-  pos += (ppos-pos)*0.1;	
+  pos += (ppos - pos) * 0.1;
 }
+
 function mouseWheel(event) {
-  print(event.delta);
+  print("delta :" + event.delta + "\t pos :" + pos);
   //move the square according to the vertical scroll amount
-	if(event.delta < 0){
-		if(event.delta>=-spread)
-			ppos -= event.delta;
-		else
-			ppos += spread;
-	}
-	else {
-		if(event.delta<=spread)
-  			ppos -= event.delta;
-		else 
-			ppos -= spread;
-	}
+  if (event.delta < 0) {
+    if (event.delta*2 >= -spread)
+      ppos -= event.delta*2;
+    else
+      ppos += spread;
+  } else {
+    if (event.delta*2 <= spread)
+      ppos -= event.delta*2;
+    else
+      ppos -= spread;
+  }
   //uncomment to block page scrolling
   //return false;
 }
-function Wait(A, B, C) {
-  this.orgY =B;
+
+function Wait(A, B, C, E) {
+  this.orgY = B;
   this.x = A;
   this.y = B;
   this.flag = C;
+  this.num = E;
   this.update = function(D) {
-    this.y = (this.orgY+D) % (24*spread);
-	if(this.y >= -(spread*12)+height/2+24*spread){
-		this.y = -spread*13;
-	}else if(this.y <= -spread*13){
-		this.y = -(spread*12)+height/2+24*spread;
-	}
+    if (this.orgY + D < 0) {
+      this.y = - (((abs(this.orgY + D)) % (120 * spread)));
+    } else {
+      this.y = (this.orgY + D) % (120 * spread);
+    }
+    
+    // if (this.y >= -(spread * 12) + height / 2 + 24 * spread) {
+    //   this.y = -spread * 13;
+    // } else if (this.y <= -spread * 13) {
+    //   this.y = -(spread * 12) + height / 2 + 24 * spread;
+    // }
   }
   this.show = function() {
     push();
     strokeWeight(10);
     translate(this.x, this.y);
+    textSize(50);
+    text(this.num, 0, 0);
     rotate(radians(30 * this.flag))
 
     for (var s = 0; s < 12; s++) {
@@ -78,11 +89,12 @@ function touchMoved(){
 	console.log(mouseY);
 	pos += (mouseY-pmouseY);
 }*/
-function touchMoved(event){
-return false;
+function touchMoved(event) {
+  return false;
 }
-function mouseDragged(){
-	fill(0);
-		//console.log(mouseX);
+
+function mouseDragged() {
+  fill(0);
+  //console.log(mouseX);
 
 }

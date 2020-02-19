3761bc15bfc42337ded9826aec315a83f50fd141
aprilcoffee <8765498@gmail.com>
2020-02-19 19:09:37 +0100

    update so far with scroll

--- scroll/sketch.js
+++ scroll/sketch.js
@@ -1,20 +1,26 @@
 var wait = [];
 //var pos = 25;
 var spread = 280;
-var pos = 0;
+var pos = -280 * 6;
+var fixed_pos = pos;
 var ppos = pos;
+var currentColor = 255;
+var speed = 0;
+var rolling = false;
 
 function setup() {
-  createCanvas(window.innerWidth - 50, window.innerHeight - 50);
+  createCanvas(window.innerWidth, window.innerHeight);
   background(255);
-  for (var s = 0; s < 120; s++) {
-    wait.push(new Wait(width / 2, -(12 * spread) + height / 2 + (s * spread), s % 12, s));
+  for (var s = 0; s < 480; s++) {
+    wait.push(new Wait(width / 2, //-(12 * spread) +
+      (s * spread), s % 12, s));
   }
 }
 
 function draw() {
+  //console.log(pos);
   background(255);
-  for (var s = 0; s < 120; s++) {
+  for (var s = 0; s < 480; s++) {
     wait[s].update(pos);
     wait[s].show();
   }
@@ -25,23 +31,55 @@ function draw() {
   //line(width/2+loc,height/2-loc,width/2+loc,height/2-loc+50);
   //line(width/2-loc,height/2+loc,width/2-loc+50,height/2+loc);
   //line(width/2-loc,height/2+loc,width/2-loc,height/2+loc-50);
-  pos += (ppos - pos) * 0.1;
+  pos += (ppos - pos) * 0.5;
 }
 
 function mouseWheel(event) {
   print("delta :" + event.delta + "\t pos :" + pos);
+  // if (speed >= spread) {
+  //   currentColor = 0;
+  //   speed *= 0.9;
+  //   if (event.delta < 0) {
+  //     ppos -= event.delta;
+  //     ppos = int(ppos / spread) * spread;
+  //     currentColor = 0;
+  //   } else {
+  //     ppos -= event.delta;
+  //     ppos = int(ppos / spread) * spread;
+  //     currentColor = 0;
+  //   }
+  // }
   //move the square according to the vertical scroll amount
+  //else {
+
+  speed = event.delta;
+
+  //Site Going Up
+  //event.Delta < 0
   if (event.delta < 0) {
-    if (event.delta*2 >= -spread)
-      ppos -= event.delta*2;
-    else
+    if (-event.delta < spread) {
+      ppos -= event.delta;
+      currentColor = map(-event.delta, 0, -spread, 255, 0);
+    } else {
       ppos += spread;
-  } else {
-    if (event.delta*2 <= spread)
-      ppos -= event.delta*2;
-    else
-      ppos -= spread;
+      ppos = int(ppos / spread) * spread;
+      currentColor = 255;
+    }
   }
+
+  //Site Going Down
+  //event.Delta > 0
+  else if (event.delta > 0) {
+    if (event.delta < spread) {
+      ppos -= event.delta;
+      currentColor = map(event.delta, 0, spread, 255, 0);
+    } else {
+      ppos -= spread;
+      ppos = int(ppos / spread) * spread;
+      currentColor = 255;
+    }
+  }
+  // }
   //uncomment to block page scrolling
   //return false;
 }
@@ -53,24 +91,30 @@ function Wait(A, B, C, E) {
   this.flag = C;
   this.num = E;
   this.update = function(D) {
-    if (this.orgY + D < 0) {
-      this.y = - (((abs(this.orgY + D)) % (120 * spread)));
-    } else {
-      this.y = (this.orgY + D) % (120 * spread);
+    this.y = this.orgY + D;
+    if (pos - this.orgY <= -12 * spread) {
+      //       this.orgY = pos + this.orgY;
     }
-    
-    // if (this.y >= -(spread * 12) + height / 2 + 24 * spread) {
-    //   this.y = -spread * 13;
-    // } else if (this.y <= -spread * 13) {
-    //   this.y = -(spread * 12) + height / 2 + 24 * spread;
+    // if (this.orgY + D < 0) {
+    //   this.Y = - (((abs(this.orgY + D)) % (120 * spread)));
+    // } else {
+    //   this.Y = (this.orgY + D) % (120 * spread);
+    // }
+
+    // if (this.y > -(spread * 12) + height / 2 + 24 * spread) {
+    //   this.orgY = -spread * 13;
+    // } else if (this.y < -spread * 13) {
+    //   this.orgY = -(spread * 12) + height / 2 + 24 * spread;
     // }
   }
   this.show = function() {
     push();
     strokeWeight(10);
-    translate(this.x, this.y);
+    translate(this.x, this.y + height / 2);
     textSize(50);
     text(this.num, 0, 0);
+    text(this.orgY, 200, -50);
+    text(this.y, 200, 50);
     rotate(radians(30 * this.flag))
 
     for (var s = 0; s < 12; s++) {
@@ -84,14 +128,14 @@ function Wait(A, B, C, E) {
     pop();
   }
 }
-/*
+
 function touchMoved(){
-	console.log(mouseY);
-	pos += (mouseY-pmouseY);
-}*/
-function touchMoved(event) {
-  return false;
+	console.log(mouseY-pmouseY);
+	ppos += (mouseY-pmouseY);
 }
+// function touchMoved(event) {
+//   return false;
+// }
 
 function mouseDragged() {
   fill(0);

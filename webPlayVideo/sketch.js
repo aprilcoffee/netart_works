6be7d512405927247109f975548a05a9b2fc6e01
aprilcoffee <8765498@gmail.com>
2021-10-18 14:49:26 +0200

    update

--- webPlayVideo/sketch.js
+++ webPlayVideo/sketch.js
@@ -1,36 +1,22 @@
-let playing = false;
-let video;
-let button;
-let capture;
-
+let cap;
 function setup() {
-	createCanvas(displayWidth, displayHeight);
-	// specify multiple formats for different browsers
-	video = createVideo(['assets/testvideo.mp4']);
-	video.hide()
-  video.play();
-	video.loop();
-
-	capture = createCapture(VIDEO);
-	capture.hide();
-	 button = createButton('play');
-	 button.mousePressed(toggleVid); // attach button listener
+  createCanvas(400, 400);
+  cap = createCapture(VIDEO);
+  cap.hide();
+  rectMode(CENTER);
+  noStroke();
 }
-
-// plays or pauses the video depending on current state
-function toggleVid() {
-	if (playing) {
-		video.pause();
-		button.html('play');
-	} else {
-		video.loop();
-		button.html('pause');
-	}
-	playing = !playing;
-}
-
 function draw() {
-	background(0);
-	image(video, 0, 0, displayWidth, displayHeight); // draw the video frame to canvas
-  image(capture, 0, 0, width, width * capture.height / capture.width);
+  background(50);
+  fill(255);
+  cap.loadPixels();
+  for (let cy = 0; cy < cap.height; cy += 10) {
+    for (let cx = 0; cx < cap.width; cx += 5) {
+      let offset = ((cy*cap.width)+cx)*4;
+      let xpos = (cx / cap.width) * width;
+      let ypos = (cy / cap.height) * height;
+      rect(xpos, ypos, 10,
+        10 * (cap.pixels[offset+1]/255));
+    }
+  }
 }
